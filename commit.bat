85f05e7532ce0bcf4f114a4652a2a2c7c8171b1f
Yoshomay <isaiah.m.lange@gmail.com>
2024-12-30 14:14:25 -0800

    cant be bothered to enter what

--- commit.bat
+++ commit.bat
@@ -1,18 +1,18 @@
 @echo off
 : adds the changed files
-set /p updatedFiles="what files?: "
-if "%updatedFiles%"=="" (
+::set /p updatedFiles="what files?: "
+::if "%updatedFiles%"=="" (
     git add *
-) else git add %updatedFiles%
+::) else git add %updatedFiles%
 
 
 : ask what
-set /p commitChanges="what changed?: " 
+::set /p commitChanges="what changed?: " 
 
 : says whats changed
-if "%commitChanges%"=="" (
+::if "%commitChanges%"=="" (
     git commit -m "cant be bothered to enter what"
-)   else git commit -m "%commitChanges%"
+::)   else git commit -m "%commitChanges%"
 
 : pushes them to the repo
 git push
